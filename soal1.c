fbfca740a7e019d65dccb2de6d6ec80164a0a06e
JevanAbiellie <jevanabiellie@gmail.com>
2026-05-06 09:12:15 +0700

    soal1.c

--- soal1.c
+++ soal1.c
@@ -8,25 +8,67 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+struct pasien{
+    char ID[10];
+    int waktu;
+    struct pasien *next;
+};
+
+struct pasien *head = NULL;
+
+void add(struct pasien **head, char ID[], int waktu){
+struct pasien *new = malloc(sizeof(struct pasien));
+    strcpy(new->ID, ID);
+    new->waktu = waktu;
+    new->next = NULL;
+    if (*head == NULL){
+        *head = new;
+        return;
+    }
+    struct pasien *temp = *head;
+    while (temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = new;
+}
+
+void tampil(struct pasien *head){
+    struct pasien *temp = head;
+    printf("ORDER ");
+    while (temp != NULL){
+        printf("%s ", temp->ID);
+        temp = temp->next;
+    }
+}
+
+void totul(struct pasien *head, int n, int *total){
+    struct pasien *temp = head;
+    int i = 0;
+    *total = 0;
+    while (temp->next != NULL){
+        *total += temp->waktu * (n-i-1);
+        temp = temp->next;
+        i++;
+    }
+    printf("WAIT %d", *total);
+    
+}
 int main(){
     int n;
+    char ID[10];
+    int waktu;
+    int total;
     scanf("%d", &n);
-    char ID[n][10];
-    int time[10];
-    int total = 0;
     for (int i = 0; i < n; i++){
-        scanf("%s\n",  &ID[i]);
-        scanf("%d", &time[i]);
+        scanf("%s\n", &ID);
+        scanf("%d", &waktu);
+        add(&head, ID, waktu);
     }
-    for (int i = 1; i < n-1; i++){
-        total += time[i] * (n-i);
-    }
-     printf("ORDER " );
-    for (int i = 0; i < n; i++){
-        printf("%s ", ID[i]);
-    }
-    printf("\nWAIT ");
-    printf("%d", total);
+    tampil(head);
+    totul(head, n, &total);
+
 }
-    
+//referensi : https://stackoverflow.com/questions/60027278/how-would-i-scan-a-string-of-unknown-length-in-c-is-there-a-way-to-do-without-u
